aac7c9be642837f99c6fe70ad8338efa3c7a9e61
Romas Navasinskas <r.navasinskas@paysera.com>
2019-10-30 15:50:31 +0200

    Expose lock and unlock actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,10 +2,14 @@ import save from './save';
 import revert from './revert';
 import saveAndLock from './saveAndLock';
 import revertAndUnlock from './revertAndUnlock';
+import lock from './lock';
+import unlock from './unlock';
 
 export {
     save,
     revert,
     saveAndLock,
     revertAndUnlock,
+    lock,
+    unlock,
 };
--- src/constants/actionTypes.js
+++ src/constants/actionTypes.js
@@ -4,10 +4,14 @@ const SAVE = `${prefix}/SAVE`;
 const REVERT = `${prefix}/REVERT`;
 const SAVE_AND_LOCK = `${prefix}/SAVE_AND_LOCK`;
 const REVERT_AND_UNLOCK = `${prefix}/REVERT_AND_UNLOCK`;
+const LOCK = `${prefix}/LOCK`;
+const UNLOCK = `${prefix}/UNLOCK`;
 
 export {
     SAVE,
     REVERT,
     SAVE_AND_LOCK,
     REVERT_AND_UNLOCK,
+    LOCK,
+    UNLOCK,
 };
